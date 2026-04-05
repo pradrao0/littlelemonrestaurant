2f4ca243f81672eb10623fa1ab3016ad0ac20bdc
Pradhyumna <pradh.rao0@gmail.com>
2026-04-05 18:03:34 +0530

    changes

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurant'
+    'menu',
+    'rest_framework',
+    'djoser',
+
 ]
 
 MIDDLEWARE = [
@@ -116,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
